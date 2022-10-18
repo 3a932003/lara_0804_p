a6abfa6059c8c9ded8bb7532943c640e73fcff4a
xu qian su <s3a932003@student.ncut.edu.tw>
2022-10-18 20:31:49 +0800

    require __DIR__.'/vendor/autoload.php';

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__.'/bootstrap.php';
-
+require __DIR__.'/vendor/autoload.php';
 // connect to dabase
 try {
     $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
